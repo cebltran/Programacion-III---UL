f1f7ea0d94f06c64a2c84d57b5d46fb342ddb6ad
cebltran <74554890+cebltran@users.noreply.github.com>
2020-12-18 21:22:20 -0500

    Publivapack
    primera version

---
+++ PublicaPlaca/Readme.md
@@ -0,0 +1,1 @@
+#Paquete de vara realizar test de placas
---
+++ PublicaPlaca/Setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="PLacVehi",
+    packages=["package"],
+    version="0.1",
+    description="Testea programa de placas",
+    author="Cesar",
+    author_email="elcontacto@goiy.com"
+    url="",
+    keywords=["**"]
+)
